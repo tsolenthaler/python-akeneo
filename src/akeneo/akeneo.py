397b3861d87830cbfe22a31f528fcbd9a9048c70
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-03-22 06:11:01 +0100

    fix limit

--- src/akeneo/akeneo.py
+++ src/akeneo/akeneo.py
@@ -81,7 +81,7 @@ class Akeneo:
         headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer '+ self.accessToken}
         nextLink = ''
         starturl = '/api/rest/v1/products'
-        starturl += '?pagination_type=search_after&limit=20'
+        starturl += '?pagination_type=search_after&limit=100'
         if len(search) > 2:
             starturl += '&search='+search
         while nextPage:
